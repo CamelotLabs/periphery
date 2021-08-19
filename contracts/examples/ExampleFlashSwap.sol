dedc0d5eea7c13b05ce2c6274f545a242625259a
percivalExcalibur <percival@excalibur.exchange>
2021-08-19 23:13:35 +0200

    init

---
+++ contracts/examples/ExampleFlashSwap.sol
@@ -0,0 +1,43 @@
+pragma solidity =0.6.6;
+
+import 'excalibur-core/contracts/interfaces/IUniswapV2Callee.sol';
+
+import '../libraries/UniswapV2Library.sol';
+import '../interfaces/V1/IUniswapV1Factory.sol';
+import '../interfaces/V1/IUniswapV1Exchange.sol';
+import '../interfaces/IUniswapV2Router01.sol';
+import '../interfaces/IERC20.sol';
+import '../interfaces/IWETH.sol';
+
+contract ExampleFlashSwap is IUniswapV2Callee {
+    address immutable factory;
+    IWETH immutable WETH;
+
+    constructor(address _factory, address router) public {
+        factory = _factory;
+        WETH = IWETH(IUniswapV2Router01(router).WETH());
+    }
+
+    // needs to accept ETH from any V1 exchange and WETH. ideally this could be enforced, as in the router,
+    // but it's not possible because it requires a call to the v1 factory, which takes too much gas
+    receive() external payable {}
+
+    // gets tokens/WETH via a V2 flash swap, swaps for the ETH/tokens on V1, repays V2, and keeps the rest!
+    function uniswapV2Call(address sender, uint amount0, uint amount1, bytes calldata data) external override {
+        address[] memory path = new address[](2);
+        uint amountToken;
+        uint amountETH;
+        { // scope for token{0,1}, avoids stack too deep errors
+        address token0 = IExcaliburV2Pair(msg.sender).token0();
+        address token1 = IExcaliburV2Pair(msg.sender).token1();
+        assert(msg.sender == UniswapV2Library.pairFor(factory, token0, token1)); // ensure that msg.sender is actually a V2 pair
+        assert(amount0 == 0 || amount1 == 0); // this strategy is unidirectional
+        path[0] = amount0 == 0 ? token0 : token1;
+        path[1] = amount0 == 0 ? token1 : token0;
+        amountToken = token0 == address(WETH) ? amount1 : amount0;
+        amountETH = token0 == address(WETH) ? amount0 : amount1;
+        }
+
+        assert(path[0] == address(WETH) || path[1] == address(WETH)); // this strategy only works with a V2 WETH pair
+    }
+}
