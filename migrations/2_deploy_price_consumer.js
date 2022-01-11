a11a1a4270322b8dcdf905ef3bdf2a14e5f9a3c9
percivalExcalibur <percival@excalibur.exchange>
2022-01-11 20:57:23 +0100

    truffle: adding avalancheLocal network

--- migrations/2_deploy_price_consumer.js
+++ migrations/2_deploy_price_consumer.js
@@ -7,15 +7,13 @@ module.exports = async function (deployer, network, accounts) {
   let usdAddress = process.env.USD_ADDRESS.toString().trim();
   let usdDecimals = process.env.USD_DECIMALS.toString().trim();
 
-  if(network === 'testnet' || network === 'development'){
+  if(network === 'avalancheLocal' || network === 'development'){
     factoryAddress = process.env.FACTORY_ADDRESS_TESTNET.toString().trim();
     excAddress = process.env.EXC_ADDRESS_TESTNET.toString().trim();
     wethAddress = process.env.WETH_ADDRESS_TESTNET.toString().trim();
     usdAddress = process.env.USD_ADDRESS_TESTNET.toString().trim();
     usdDecimals = process.env.USD_DECIMALS_TESTNET.toString().trim();
   }
-
-
 
   await deployer.deploy(PriceConsumer, factoryAddress, wethAddress, usdAddress, excAddress, usdDecimals)
 };
--- truffle-config.js
+++ truffle-config.js
@@ -42,22 +42,13 @@ module.exports = {
       port: 8545,            // Standard BSC port (default: none)
       network_id: "*",       // Any network (default: none)
     },
-//    testnet: {
-//      provider: () => new HDWalletProvider(MNENOMIC, `https://data-seed-prebsc-2-s3.binance.org:8545/`),
-//      network_id: 97,
-//      confirmations: 2,
-//      timeoutBlocks: 1200,
-//      skipDryRun: true,
-//      from: process.env.DEPLOYER_ADDRESS.toString().trim(),
-//    },
-//    bsc: {
-//      provider: () => new HDWalletProvider(MNENOMIC, `https://bsc-dataseed.binance.org/`),
-//      network_id: 56,
-//      confirmations: 10,
-//      timeoutBlocks: 1200,
-//      skipDryRun: true,
-//      from: process.env.DEPLOYER_ADDRESS.toString().trim(),
-//    },
+    avalancheLocal: {
+      provider: () => new HDWalletProvider(MNENOMIC, 'http://127.0.0.1:9650/ext/bc/C/rpc'),
+      network_id: "*",
+      timeoutBlocks: 1200,
+      skipDryRun: true,
+      from: process.env.DEPLOYER_ADDRESS.toString().trim(),
+    },
   },
 
   mocha: {
@@ -86,7 +77,7 @@ module.exports = {
        settings: {          // See the solidity docs for advice about optimization and evmVersion
         optimizer: {
           enabled: true,
-          runs: 999999
+          runs: 39999
         }
       }
     }
